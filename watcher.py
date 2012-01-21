b899456393c94fcf4550a19b9d1cbccb2db1842a
Wessie <Wessie@Wessie.info>
2012-01-21 03:31:53 +0100

    Removed proxy-module.py reasons naming
    Fixed the strange MySQLCursor in watcher.py

--- watcher.py
+++ watcher.py
@@ -16,33 +16,32 @@ def start_watcher():
 def stop_watcher():
 	notifier.stop()
 def parse_queue_file():
-	with web.MySQLCursor() as cur:
-		queue = []
-		remaining = 0
-		with open(path.join(config.watcher_path, config.watcher_file)) as file:
-			djid = file.readline().strip()
-			firstline = file.readline().strip()
-			
-			if (djid != ''):
-				remaining = int(firstline)
-				current_djid = webcom.get_djid()
-				if (current_djid == djid):
-					for line in file:
-						line = line.strip()
-						if line == "":
-							continue
-						
-						try:
-							spacepos = line.index(' ')
-						except:
-							continue
-						stime = int(line[:spacepos])
-						song = line[spacepos+1:]
-						
-						song = webcom.fix_encoding(song)
-						queue.append((stime, song))
-		if (len(queue) > 0):
-			webcom.send_queue(remaining, queue)
+	queue = []
+	remaining = 0
+	with open(path.join(config.watcher_path, config.watcher_file)) as file:
+		djid = file.readline().strip()
+		firstline = file.readline().strip()
+		
+		if (djid != ''):
+			remaining = int(firstline)
+			current_djid = webcom.get_djid()
+			if (current_djid == djid):
+				for line in file:
+					line = line.strip()
+					if line == "":
+						continue
+					
+					try:
+						spacepos = line.index(' ')
+					except:
+						continue
+					stime = int(line[:spacepos])
+					song = line[spacepos+1:]
+					
+					song = webcom.fix_encoding(song)
+					queue.append((stime, song))
+	if (len(queue) > 0):
+		webcom.send_queue(remaining, queue)
 			
 class handler(pi.ProcessEvent):
 	def process_IN_MODIFY(self, event):
