2c82a28ee2e5a19211cc76cefb58ae47241edb11
Wessie <Wessie@Wessie.info>
2012-01-21 03:19:39 +0100

    ProxyModule

--- ProxyModule.py
+++ ProxyModule.py
@@ -1,18 +1,26 @@
 import logging
 
 loaded_modules = {}
+loaded_proxies = {}
 def get(name):
     if (loaded_modules.has_key(name)):
-        return loaded_modules[name]
+        return loaded_proxies[name]
     else:
         try:
             module = __import__(name)
         except (ImportError):
             logging.critical("Attempted module get: {name}".format(name))
-            return ProxyModule(None)
-        return ProxyModule(module)
-
-class ProxyModule:
+            proxy = ProxyModule(None)
+        else:
+            proxy = ProxyModule(module)
+            loaded_modules[name] = module
+        loaded_proxies[name] = proxy
+        return proxy
+class ProxyModule(object):
+    """Wraps around a module, so that the module can be reloaded without
+    the user noticing this.
+     
+     """
     def __init__(self, module):
         self.__proxy_module = module
     def __getattr__(self, name):
