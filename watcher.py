15a2dd39f2658f4aaed2f9b5a048adc3997f538b
Wessie <Wessie@Wessie.info>
2012-02-29 03:49:44 +0100

    watcher.py changed to accompany the rules of module start/shutdown process, also changed from tabs to spaces indentation.

--- watcher.py
+++ watcher.py
@@ -7,49 +7,49 @@ import logging
 import manager
 from os import path
 
-def start_watcher():
-	global notifier
-	wm = pi.WatchManager()
-	notifier = pi.ThreadedNotifier(wm, handler())
-	notifier.start()
-	wdd = wm.add_watch(config.watcher_path, pi.IN_MODIFY)
-	
-def stop_watcher():
-	notifier.stop()
+def start():
+    global notifier
+    wm = pi.WatchManager()
+    notifier = pi.ThreadedNotifier(wm, handler())
+    notifier.start()
+    wdd = wm.add_watch(config.watcher_path, pi.IN_MODIFY)
+    
+def shutdown():
+    notifier.stop()
+    
 def parse_queue_file():
-	queue = []
-	remaining = 0
-	with open(path.join(config.watcher_path, config.watcher_file)) as f:
-		djid = f.readline().strip()
-		firstline = f.readline().strip()
-		
-		if (djid != ''):
-			remaining = int(firstline)
-			current_djid = webcom.get_djid()
-			if (current_djid == djid):
-				for line in file:
-					line = line.strip()
-					if line == "":
-						continue
-					
-					try:
-						spacepos = line.index(' ')
-					except:
-						continue
-					stime = int(line[:spacepos])
-					song = line[spacepos+1:]
-					
-					song = webcom.fix_encoding(song)
-					queue.append(manager.Song(meta=song, length=stime))
-			else:
-				logging.info("Queue discarded because djid does not match")
-	if (len(queue) > 0):
-		manager.queue.clear()
-		manager.np.remaining(remaining)
-		manager.queue.append_many(queue)
-			
+    queue = []
+    remaining = 0
+    with open(path.join(config.watcher_path, config.watcher_file)) as f:
+        djid = f.readline().strip()
+        firstline = f.readline().strip()
+        
+        if (djid != ''):
+            remaining = int(firstline)
+            current_djid = webcom.get_djid()
+            if (current_djid == djid):
+                for line in file:
+                    line = line.strip()
+                    if line == "":
+                        continue
+                    
+                    try:
+                        spacepos = line.index(' ')
+                    except:
+                        continue
+                    stime = int(line[:spacepos])
+                    song = line[spacepos+1:]
+                    
+                    song = webcom.fix_encoding(song)
+                    queue.append(manager.Song(meta=song, length=stime))
+            else:
+                logging.info("Queue discarded because djid does not match")
+    if (len(queue) > 0):
+        manager.queue.clear()
+        manager.np.remaining(remaining)
+        manager.queue.append_many(queue)
+            
 class handler(pi.ProcessEvent):
-	def process_IN_MODIFY(self, event):
-		if (event.name == 'queue.txt'):
-			parse_queue_file()
-
+    def process_IN_MODIFY(self, event):
+        if (event.name == 'queue.txt'):
+            parse_queue_file()
