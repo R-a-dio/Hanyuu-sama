f0c97091c2cda8bdd7e99a563506700976fd3853
Wessie <Wessie@Wessie.info>
2012-03-01 03:38:36 +0100

    Added lock to the website requests, due to a bug that makes you able to request multiple songs if done right

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ djfile = './dj.conf' # Where to get the list of DJs
 import logging
 logging.basicConfig(level=logging.INFO)
 handle = ''
+
+request_lock = threading.RLock()
 class HanyuuHandlers:
 	def __init__(self):
 		self.__handlers = {}
@@ -69,77 +71,78 @@ class HanyuuHandlers:
 
 def external_request(environ, start_response):
 	if (shout.afk_streaming):
-		def is_int(num):
+		with request_lock:
+			def is_int(num):
+				try:
+					int(num)
+					return True
+				except:
+					return False
 			try:
-				int(num)
-				return True
+				postdata = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
 			except:
-				return False
-		try:
-			postdata = environ['wsgi.input'].read(int(environ['CONTENT_LENGTH']))
-		except:
-			postdata = ""
-		splitdata = postdata.split('=')
-		sitetext = ""
-		if len(splitdata) == 2 and splitdata[0] == 'songid' and is_int(splitdata[1]):
-			songid = int(splitdata[1])
-			canrequest_ip = False
-			canrequest_song = False
-			with web.MySQLCursor() as cur:
-				# SQL magic
-				cur.execute("SELECT * FROM `requesttime` WHERE `ip`='%s' LIMIT 1;" % (environ["REMOTE_ADDR"]))
-				ipcount = cur.rowcount
-				if cur.rowcount >= 1:
-					try:
-						iptime = int(time.mktime(time.strptime(str(cur.fetchone()["time"]), "%Y-%m-%d %H:%M:%S" )))
-					except:
-						iptime = 0
-				else:
-					iptime = 0
-				now = int(time.time())
-				if now - iptime > 1800:
-					canrequest_ip = True
-				
-				cur.execute("SELECT * FROM `tracks` WHERE `id`=%s LIMIT 1;" % (songid))
-				if cur.rowcount >= 1:
-					try:
-						lptime = int(time.mktime(time.strptime(str(cur.fetchone()["lastrequested"]), "%Y-%m-%d %H:%M:%S" )))
-					except:
-						lptime = 0
-				else:
-					lptime = now
-				if now - lptime > 3600 * 8:
-					canrequest_song = True
-				
-				if cur.rowcount >= 1:
-					try:
-						lptime = int(time.mktime(time.strptime(str(cur.fetchone()["lastplayed"]), "%Y-%m-%d %H:%M:%S" )))
-					except:
-						lptime = 0
-				else:
-					lptime = now
-				if now - lptime > 3600 * 8:
-					canrequest_song = canrequest_song and True
-
-				
-				if not canrequest_ip or not canrequest_song:
-					if not canrequest_ip:
-						sitetext = "You need to wait longer before requesting again."
-					elif not canrequest_song:
-						sitetext = "You need to wait longer before requesting this song."
-				else:
-					sitetext = "Thank you for making your request!"
-					#SQL magic
-					if ipcount >= 1:
-						cur.execute("UPDATE `requesttime` SET `time`=NOW() WHERE `ip`='%s';" % (environ["REMOTE_ADDR"]))
+				postdata = ""
+			splitdata = postdata.split('=')
+			sitetext = ""
+			if len(splitdata) == 2 and splitdata[0] == 'songid' and is_int(splitdata[1]):
+				songid = int(splitdata[1])
+				canrequest_ip = False
+				canrequest_song = False
+				with web.MySQLCursor() as cur:
+					# SQL magic
+					cur.execute("SELECT * FROM `requesttime` WHERE `ip`='%s' LIMIT 1;" % (environ["REMOTE_ADDR"]))
+					ipcount = cur.rowcount
+					if cur.rowcount >= 1:
+						try:
+							iptime = int(time.mktime(time.strptime(str(cur.fetchone()["time"]), "%Y-%m-%d %H:%M:%S" )))
+						except:
+							iptime = 0
 					else:
-						cur.execute("INSERT INTO `requesttime` (`ip`) VALUES ('%s');" % (environ["REMOTE_ADDR"]))
-					cur.execute("UPDATE `tracks` SET `lastrequested`=NOW(), `priority`=priority+4 WHERE `id`=%s;" % (songid))
-					main.irc_request_announce(songid)
-					shout.queue.add_request(songid)
-					shout.queue.send_queue(shout.get_left())
-		else:
-			sitetext = "Invalid parameter."
+						iptime = 0
+					now = int(time.time())
+					if now - iptime > 1800:
+						canrequest_ip = True
+					
+					cur.execute("SELECT * FROM `tracks` WHERE `id`=%s LIMIT 1;" % (songid))
+					if cur.rowcount >= 1:
+						try:
+							lptime = int(time.mktime(time.strptime(str(cur.fetchone()["lastrequested"]), "%Y-%m-%d %H:%M:%S" )))
+						except:
+							lptime = 0
+					else:
+						lptime = now
+					if now - lptime > 3600 * 8:
+						canrequest_song = True
+					
+					if cur.rowcount >= 1:
+						try:
+							lptime = int(time.mktime(time.strptime(str(cur.fetchone()["lastplayed"]), "%Y-%m-%d %H:%M:%S" )))
+						except:
+							lptime = 0
+					else:
+						lptime = now
+					if now - lptime > 3600 * 8:
+						canrequest_song = canrequest_song and True
+	
+					
+					if not canrequest_ip or not canrequest_song:
+						if not canrequest_ip:
+							sitetext = "You need to wait longer before requesting again."
+						elif not canrequest_song:
+							sitetext = "You need to wait longer before requesting this song."
+					else:
+						sitetext = "Thank you for making your request!"
+						#SQL magic
+						if ipcount >= 1:
+							cur.execute("UPDATE `requesttime` SET `time`=NOW() WHERE `ip`='%s';" % (environ["REMOTE_ADDR"]))
+						else:
+							cur.execute("INSERT INTO `requesttime` (`ip`) VALUES ('%s');" % (environ["REMOTE_ADDR"]))
+						cur.execute("UPDATE `tracks` SET `lastrequested`=NOW(), `priority`=priority+4 WHERE `id`=%s;" % (songid))
+						main.irc_request_announce(songid)
+						shout.queue.add_request(songid)
+						shout.queue.send_queue(shout.get_left())
+			else:
+				sitetext = "Invalid parameter."
 	else:
 		sitetext = "You can't request songs at the moment."
 	start_response('200 OK', [('Content-Type', 'text/html')])
