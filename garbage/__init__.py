2e67ffa56be5029538644bfce4e6f41bf0e72b82
Wessie <Wessie@Wessie.info>
2012-10-26 00:12:27 +0200

    Simple higher level garbage collector added.
    This is a very simple interface that runs a threaded `Collector` that
    calls a `collect` method on all items that are being tracked. If the
    method returns True the `Collector` assumes everything was cleaned and
    will remove the object from tracking.
    
    Each item is a subclass of `Garbage`.
    
    This instance is ment to keep track of incorrectly closed threads,
    subprocesses or other pieces that don't always clean properly.

---
+++ garbage/__init__.py
@@ -0,0 +1,66 @@
+import threading
+import logging
+import time
+
+
+logger = logging.getLogger('garbage')
+
+
+class Singleton(type):
+    def __init__(mcs, name, bases, dict):
+        super(Singleton, mcs).__init__(name, bases, dict)
+        mcs.instance = None
+
+    def __call__(mcs, *args, **kw):
+        if mcs.instance is None:
+            mcs.instance = super(Singleton, mcs).__call__(*args, **kw)
+        return mcs.instance
+    
+    
+class Collector(object):
+    __metaclass__ = Singleton
+    def __init__(self):
+        super(Collector, self).__init__()
+        self.items = set()
+        
+        self.thread = threading.Thread(target=self.run,
+                                       name="Garbage Collection Thread")
+        self.thread.start()
+        
+    def add(self, garbage):
+        self.items.add(garbage)
+        
+    def run(self):
+        while not self.collecting.is_set():
+            removal = set()
+            for item in self.items:
+                try:
+                    code = item.collect() # Try collecting
+                except:
+                    logger.exception("Collection Failure.")
+                else:
+                    if code: # If it returned True it was successful
+                        removal.add(item)
+            self.items -= removal # We remove our set from the item one
+            time.sleep(15.0)
+            
+    def info(self):
+        """Returns a list of GarbageInfo objects containing information
+        about current pending garbage."""
+        yield None # Not implemented
+        
+        
+class Garbage(object):
+    collector = Collector()
+    def __init__(self, item=None):
+        super(Garbage, self).__init__()
+        self.item = item
+        self.collector.add(self)
+        
+    def collect(self):
+        """Gets called on each collection cycle. 
+        
+        Should return True if the garbage got cleaned up properly,
+        False if it requires another collect in the next cycle.
+        """
+        raise NotImplementedError("collect method not overridden.")
