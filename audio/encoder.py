03eb7e33135637ae17d6b0bc4298dbb79bd85d5e
Wessie <Wessie@Wessie.info>
2012-10-26 16:14:28 +0200

    Encoder split into two classes. One is the manager for encoders.
    By splitting the logic it's easier to restart a death encoder.

--- audio/encoder.py
+++ audio/encoder.py
@@ -3,23 +3,76 @@ import threading
 import decimal
 import time
 import select
+import logging
+import garbage
 
 
 LAME_BIN = 'lame'
+logger = logging.getLogger('audio.encoder')
 
 
 class EncodingError(Exception):
     pass
-
+        
 
 class Encoder(object):
+    """An Encoder that handles the encoder process underneath.
+    
+    It is possible that the actual process to encode with is different
+    over time due to crashes or restarts
+    """
     def __init__(self, source):
         super(Encoder, self).__init__()
+        self.alive = threading.Event()
+        
         self.source = source
         self.compression = ['--cbr', '-b', '192', '--resample', '44.1']
         self.mode = 'j'
         
         self.out_file = '-'
+        
+    def start(self):
+        self.start_instance()
+        
+    def close(self):
+        """Closes the encoder."""
+        self.alive.set() # Set ourself to closed so we don't restart instances
+        self.instance.close()
+        
+    def restart(self):
+        """Restarts the encoder process underneath."""
+        self.report_close() # Hackish way of restarting
+        
+    def report_close(self):
+        """Called when EncoderInstance is closed
+        
+        This registers the current instance for garbage collection and
+        then starts a new instance for use.
+        """
+        if not self.alive.is_set():
+            GarbageInstance(self.instance)
+            self.start_instance()
+            
+    def start_instance(self):
+        """Called to create a new EncoderInstance"""
+        new = EncoderInstance(self)
+        new.start()
+        self.instance = new
+        
+    def __getattr__(self, key):
+        """Since we are used as the source to other parts we require
+        to have direct access to EncoderInstance methods from ourself."""
+        return getattr(self.instance, key)
+    
+    
+class EncoderInstance(object):
+    """Class that represents a subprocessed encoder."""
+    def __init__(self, encoder_manager):
+        super(EncoderInstance, self).__init__()
+        self.encoder_manager = encoder_manager
+        
+        for key in ['source', 'compression', 'mode', 'out_file']:
+            setattr(self, key, getattr(self.encoder_manager, key))
         
         self.running = threading.Event()
         
@@ -30,6 +83,12 @@ class Encoder(object):
                 # EOF we just sleep and wait for a new source
                 time.sleep(0.3)
             self.write(data)
+        try:
+            self.process.stdin.close()
+            self.process.stdout.close()
+            self.process.wait()
+        except:
+            logger.exception("Failed to cleanly shutdown encoder.")
             
     def start(self):
         arguments = [LAME_BIN, '--quiet',
@@ -44,10 +103,10 @@ class Encoder(object):
                                         stdin=subprocess.PIPE,
                                         stdout=subprocess.PIPE)
         
-        self.feed_thread = threading.Thread(target=self.run,
+        self.thread = threading.Thread(target=self.run,
                                             name='Encoder Feeder')
-        self.feed_thread.daemon = True
-        self.feed_thread.start()
+        self.thread.daemon = True
+        self.thread.start()
         
     def switch_source(self, new_source):
         self.source = new_source
@@ -56,14 +115,12 @@ class Encoder(object):
         try:
             self.process.stdin.write(data)
         except (IOError, ValueError) as err:
-            self.process.stdin.close()
-            self.process.stdout.close()
-            self.process.wait()
-            raise EncodingError(str(err))
+            logger.exception("Write failed, restarting encoder.")
+            self.close()
+            #raise EncodingError(str(err))
         except (Exception) as err:
-            self.process.stdin.close()
-            self.process.stdout.close()
-            self.process.wait()
+            logger.exception("Write failed, unknown exception.")
+            self.close()
             raise err
         
     def read(self, size=4096, timeout=10.0):
@@ -75,3 +132,16 @@ class Encoder(object):
     
     def close(self):
         self.running.set()
+        self.encoder_manager.report_close()
+        
+        
+class GarbageInstance(garbage.Garbage):
+    def collect(self):
+        # Check if our encoder process is down yet
+        returncode = self.item.process.poll()
+        
+        self.item.thread.join(0.0) # Check if thread can be joined.
+        
+        if self.item.thread.isAlive() or returncode is None:
+            return False
+        return True
