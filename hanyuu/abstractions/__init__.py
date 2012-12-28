14868a2bcfe0f75cc8a7e2eef9417affe1b92c2a
Wessie <wessie@r-a-d.io>
2012-12-28 21:54:53 +0100

    Moved encapsulations to it's own subpackage and renamed to abstractions.
    See documentation for developer notes.

---
+++ hanyuu/abstractions/__init__.py
@@ -0,0 +1,15 @@
+"""
+package to abstract the database from the rest of the code.
+
+For users:
+    submodules are grouped by their overarcing thema such as DJ profiles and
+    users in the same submodule, track information in the same submodule,
+    AFK streamer information in the same submodule, etc.
+    
+For developers:
+    submodules should be of the grouping type where closely related data
+    structures are placed together in a module.
+"""
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
---
+++ hanyuu/abstractions/users.py
@@ -0,0 +1,61 @@
+"""
+A module used for the abstractions of the users part of the database.
+"""
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+from ..db import models
+from .. import utils, config
+
+
+@utils.instance_decorator
+class DJ(object):
+    """
+    Encapsulates the concept of a DJ in our system.
+    
+    This abstracts the database from the rest of the code. But does return a
+    database related object since it's a simple one.
+    """
+    def __init__(self, id=None, name=None):
+        """
+        Returns a read-only DJ object.
+        
+        Raises DoesNotExist if no match was found or ValueError if no arguments
+        where supplied.
+        """
+        if id is None and name is None:
+            raise ValueError("Neither `name` nor `id` is supplied.")
+        elif id is None:
+            self.model = models.DJ.get(models.DJ.name == name)
+        elif name is None:
+            self.model = models.DJ.get(models.DJ.id == id)
+        else:
+            self.model = models.DJ.get(models.DJ.id == id, models.DJ.name == name)
+        
+    def __getattr__(self, key):
+        """
+        This method is chosen to add abstraction, if we change the database
+        layout or other things we can easily adopt it into the older structure
+        by adding properties.
+        """
+        return getattr(self.model, key)
+    
+    @classmethod
+    def resolve_name(cls, name):
+        """
+        Resolves a DJ username to a DJ identifier.
+        
+        Returns an integer that is the DJ identifier or 0 if the DJ username
+        does not exist.
+        """
+        return cls(name=name)
+        
+    @classmethod
+    def resolve_id(cls, id):
+        """
+        Resolves a DJ identifier to a DJ username.
+        
+        Returns a class instance or raises DoesNotExist
+        """
+        return cls(id=id)
+    
