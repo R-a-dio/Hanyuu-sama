7f816ccd12410f908d5b1f7c97ac775b74510725
Wessie <wessie@r-a-d.io>
2013-01-04 17:38:06 +0100

    Added extra mocking for pylibmc.

--- tests/status/__init__.py
+++ tests/status/__init__.py
@@ -5,5 +5,16 @@ from __future__ import absolute_import
 
 # Mock the memcache server to a test server
 from .. import mock
-import pylibmc
+try:
+    import pylibmc
+except ImportError:
+    # We don't have the actual pylibmc installed so mock it even uglier.
+    class Mocker(object):
+        pass
+    pylibmc = Mocker()
+    import sys
+    sys.modules['pylibmc'] = pylibmc
+
+    print("WARNING: Using mocked pylibmc with reduced functionality instead.")
+
 pylibmc.Client = mock.MockMemcacheClient
