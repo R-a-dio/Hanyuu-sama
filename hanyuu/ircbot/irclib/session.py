3ed456fc48658243f5cc45ffeafb4ee90a3fd995
Wessie <wessie@r-a-d.io>
2013-01-11 02:11:27 +0100

    Moved around utility functions to utils.py
    Did more small work on the event abstraction.

--- hanyuu/ircbot/irclib/session.py
+++ hanyuu/ircbot/irclib/session.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
+from __future__ import absolute_imports
 """
 A high level session object to the lower level irclib.connection module.
 """
-
+from . import utils
 
 class Session(object):
     """
@@ -13,56 +14,149 @@ class Session(object):
         super(Session, self).__init__()
         self.irclib = connection.IRC()
         
-    def high_level_handler(self, event):
+    def low_level_handler(self, event):
         """
         Registered handler for all lowlevel events.
         
         This should make higher level abstractions of the Event type passed.
         
         """
-        command = event.eventtype
+        event = HighEvent.from_low_event(event)
+
+
+class HighEvent(object):
+    """
+    A abstracted event of the IRC library.
+    """
+    def __init__(self, server, nickname, channel, message):
+        super(HighEvent, self).__init__()
+        
+        self.nickname = nickname
+        self.server = server
+        self.channel = channel
+        self.message = message
+        
+    @classmethod
+    def from_low_event(cls, low_event):
+        command = low_event.eventtype
+        
+        # We supply the source and server already to reduce code repetition.
+        # Just use it as the HighEvent constructor but with partial applied.
+        creator = lambda *args, **kwargs: cls(low_event.server,
+                                              *args,
+                                              **kwargs)
         
         if command == 'nick':
             # A nickname change.
-            pass
+            old_nickname = Nickname(low_event.source)
+            
+            # We cheat here by using the original host and replacing the
+            # name attribute with our new nickname.
+            # TODO: Make sure this works correctly with the hostmask
+            new_nickname = Nickname(low_event.source)
+            new_nickname.name = low_event.arguments[0]
+            
+            event = creator(old_nickname, None, None)
+            event.new_nickname = new_nickname
+            return event
         elif command == 'welcome':
             # Event used for the welcome message
             pass
         elif command == 'pubmsg':
             # A channel message
-            pass
+            nickname = Nickname(low_event.source)
+            channel = low_event.target
+            message = low_event.arguments[0]
+            return creator(nickname, channel, message)
+        elif command == 'privmsg':
+            # Private message
+            # The target is set to our own nickname in privmsg.
+            nickname = Nickname(low_event.source)
+            message = low_event.arguments[0]
+            return creator(nickname, None, message)
         elif command == 'pubnotice':
             # A notice to a channel
-            pass
+            nickname = Nickname(low_event.source)
+            channel = low_event.target
+            message = low_event.arguments[0]
+            return creator(nickname, channel, message)
         elif command == 'privnotice':
             # A notice to only us.
-            pass
-        elif command == 'ctcp':
+            # The target is set to our own nickname in privnotice.
+            nickname = Nickname(low_event.source)
+            message = low_event.arguments[0]
+            return creator(nickname, None, message)
+        elif command == 'ctcp' or command == 'action':
             # A CTCP to us.
-            pass
+            # Same as privmsg/notice the target is our own nickname
+            nickname = Nickname(low_event.source)
+            # The irclib splits off the first space delimited word for us.
+            command = low_event.arguments[0]
+            # The things behind the command are then indexed behind it.
+            message = low_event.arguments[1]
+            
+            event = creator(nickname, None, message)
+            event.command = command
+            return event
         elif command == 'ctcpreply':
             # A reply to one of our own CTCPs
             pass
-        elif command == 'action':
-            # An action (/me) really just a CTCP
-            pass
         elif command == 'quit':
             # A quit from an user.
-            pass
-        elif command == 'ping':
-            # A ping request
-            pass
+            nickname = Nickname(low_event.source)
+            message = low_event.arguments[0]
+            
+            return creator(nickname, None, message)
         elif command == 'join':
             # Someone joining our channel
-            pass
+            nickname = Nickname(low_event.source)
+            channel = low_event.target
+            
+            return creator(nickname, channel, None)
         elif command == 'part':
             # Someone leaving our channel
-            pass
+            nickname = Nickname(low_event.source)
+            message = low_event.arguments[0]
+            channel = low_event.target
+            
+            return creator(nickname, channel, message)
         elif command == 'kick':
             # Someone forcibly leaving our channel.
-            pass
+            # The person kicking here
+            kicker = Nickname(low_event.source)
+            # The person being kicked
+            target = Nickname(low_event.arguments[0], nickname_only=True)
+            # The reason given by the kicker
+            reason = low_event.arguments[1]
+            # The channel this all went wrong in!
+            channel = low_event.target
+            
+            event = creator(target, channel, reason)
+            event.kicker = kicker
+            return event
         elif command == 'topic':
             # Someone changing the channel topic.
+            nickname = Nickname(low_event.source)
+            channel = low_event.target
+            topic = low_event.arguments[0]
+            
+            return creator(nickname, channel, topic)
+        elif command == 'mode':
+            # Mode change in the channel
+            # The nickname that set the mode
+            mode_setter = Nickname(low_event.source)
+            # Simple channel
+            channel = low_event.target
+            # This is a raw mode string that we need to parse and add to
+            # the targeted individuals.
+            modes = utils.parse_modes(low_event.arguments[0])
+            # This is a list of targets instead of one item so common
+            targets = low_event.arguments[1:]
+            
+            event = creator(mode_setter, channel, None)
+            event.modes = intertwine_modes(modes, targets)
+        elif command == 'umode':
+            # A user mode was set.
             pass
         elif command == 'currenttopic':
             # A response/welcome telling us what the topic is.
@@ -80,8 +174,30 @@ class Session(object):
         elif command == 'namreply':
             # A name info reply.
             pass
-        elif command == 'mode':
-            # Mode change in the channel
-            pass
+
         # TODO: Check for missing commands.
         return
+    
+    
+class Nickname(object):
+    """
+    A simple class that represents a nickname on IRC.
+    
+    Contains information such as actual nickname, hostmask and more.
+    """
+    def __init__(self, host, nickname_only=False):
+        """
+        The constructor really just expects the raw host send by IRC servers.
+        
+        it parses this for you into segments.
+        
+        if `nickname_only` is set to True it expects a bare nickname unicode
+        object to be used as nickname and nothing more.
+        """
+        super(Nickname, self).__init__()
+        
+        if nickname_only:
+            self.name = host
+        else:
+            self.name = utils.nm_to_n(host)
+            self.host = host
---
+++ hanyuu/ircbot/irclib/utils.py
@@ -0,0 +1,240 @@
+def parse_modes(string):
+    """
+    Returns a generator that yields tuples of (operator, mode)
+    
+    >>> list(parse_modes('+bbb-a'))
+    [('+', 'b'), ('+', 'b'), ('+', 'b'), ('-', 'a')]
+    
+    """
+    mode = None
+    for char in string:
+            if char in ('-', '+'):
+                    mode = char
+            elif mode is None:
+                    raise TypeError("Invalid mode string.")
+            else:
+                    yield (mode, char)
+
+
+def intertwine_modes(modes, targets):
+    """
+    :params modes: An iterator of (operator, mode) tuples as returned by :func:`parse_modes`
+    :params targets: A list of equal size of the iterator.
+    """
+    # TODO: Actually implement this.
+    # This might need to be moved to a server specific factory
+    pass
+
+
+_LOW_LEVEL_QUOTE = "\020"
+_CTCP_LEVEL_QUOTE = "\134"
+_CTCP_DELIMITER = "\001"
+
+_low_level_mapping = {
+    "0": "\000",
+    "n": "\n",
+    "r": "\r",
+    _LOW_LEVEL_QUOTE: _LOW_LEVEL_QUOTE
+}
+
+_low_level_regexp = re.compile(_LOW_LEVEL_QUOTE + "(.)", re.UNICODE)
+
+def mask_matches(nick, mask):
+    """Check if a nick matches a mask.
+
+    Returns true if the nick matches, otherwise false.
+    """
+    nick = irc_lower(nick)
+    mask = irc_lower(mask)
+    mask = mask.replace("\\", "\\\\")
+    for ch in ".$|[](){}+":
+        mask = mask.replace(ch, "\\" + ch)
+    mask = mask.replace("?", ".")
+    mask = mask.replace("*", ".*")
+    r = re.compile(mask, re.IGNORECASE)
+    return r.match(nick)
+
+_special = "-[]\\`^{}"
+nick_characters = string.ascii_letters + string.digits + _special
+_ircstring_translation = string.maketrans(string.ascii_uppercase + "[]\\^",
+                                        string.ascii_lowercase + "{}|~")
+
+def irc_lower(s):
+    """Returns a lowercased string.
+
+    The definition of lowercased comes from the IRC specification (RFC
+    1459).
+    """
+    if (type(s) == unicode):
+        s = s.encode('utf-8')
+    return s.translate(_ircstring_translation)
+
+def _ctcp_dequote(message):
+    """[Internal] Dequote a message according to CTCP specifications.
+
+    The function returns a list where each element can be either a
+    string (normal message) or a tuple of one or two strings (tagged
+    messages).  If a tuple has only one element (ie is a singleton),
+    that element is the tag; otherwise the tuple has two elements: the
+    tag and the data.
+
+    Arguments:
+
+        message -- The message to be decoded.
+    """
+
+    def _low_level_replace(match_obj):
+        ch = match_obj.group(1)
+
+        # If low_level_mapping doesn't have the character as key, we
+        # should just return the character.
+        return _low_level_mapping.get(ch, ch)
+
+    if _LOW_LEVEL_QUOTE in message:
+        # Yup, there was a quote.  Release the dequoter, man!
+        message = _low_level_regexp.sub(_low_level_replace, message)
+
+    if _CTCP_DELIMITER not in message:
+        return [message]
+    else:
+        # Split it into parts.  (Does any IRC client actually *use*
+        # CTCP stacking like this?)
+        chunks = message.split(_CTCP_DELIMITER)
+
+        messages = []
+        i = 0
+        while i < len(chunks)-1:
+            # Add message if it's non-empty.
+            if len(chunks[i]) > 0:
+                messages.append(chunks[i])
+
+            if i < len(chunks)-2:
+                # Aye!  CTCP tagged data ahead!
+                messages.append(tuple(chunks[i+1].split(" ", 1)))
+
+            i = i + 2
+
+        if len(chunks) % 2 == 0:
+            # Hey, a lonely _CTCP_DELIMITER at the end!  This means
+            # that the last chunk, including the delimiter, is a
+            # normal message!  (This is according to the CTCP
+            # specification.)
+            messages.append(_CTCP_DELIMITER + chunks[-1])
+
+        return messages
+
+def is_channel(string):
+    """Check if a string is a channel name.
+
+    Returns true if the argument is a channel name, otherwise false.
+    """
+    return string and string[0] in "#&+!"
+
+def ip_numstr_to_quad(num):
+    """Convert an IP number as an integer given in ASCII
+    representation (e.g. '3232235521') to an IP address string
+    (e.g. '192.168.0.1')."""
+    n = long(num)
+    p = map(str, map(int, [n >> 24 & 0xFF, n >> 16 & 0xFF,
+                           n >> 8 & 0xFF, n & 0xFF]))
+    return ".".join(p)
+
+def ip_quad_to_numstr(quad):
+    """Convert an IP address string (e.g. '192.168.0.1') to an IP
+    number as an integer given in ASCII representation
+    (e.g. '3232235521')."""
+    p = map(long, quad.split("."))
+    s = str((p[0] << 24) | (p[1] << 16) | (p[2] << 8) | p[3])
+    if s[-1] == "L":
+        s = s[:-1]
+    return s
+
+def nm_to_n(s):
+    """Get the nick part of a nickmask.
+
+    (The source of an Event is a nickmask.)
+    """
+    return s.split("!")[0]
+
+def nm_to_uh(s):
+    """Get the userhost part of a nickmask.
+
+    (The source of an Event is a nickmask.)
+    """
+    return s.split("!")[1]
+
+def nm_to_h(s):
+    """Get the host part of a nickmask.
+
+    (The source of an Event is a nickmask.)
+    """
+    return s.split("@")[1]
+
+def nm_to_u(s):
+    """Get the user part of a nickmask.
+
+    (The source of an Event is a nickmask.)
+    """
+    s = s.split("!")[1]
+    return s.split("@")[0]
+
+def parse_nick_modes(mode_string):
+    """Parse a nick mode string.
+
+    The function returns a list of lists with three members: sign,
+    mode and argument.  The sign is \"+\" or \"-\".  The argument is
+    always None.
+
+    Example:
+
+    >>> irclib.parse_nick_modes(\"+ab-c\")
+    [['+', 'a', None], ['+', 'b', None], ['-', 'c', None]]
+    """
+
+    return _parse_modes(mode_string, "")
+
+def parse_channel_modes(mode_string):
+    """Parse a channel mode string.
+
+    The function returns a list of lists with three members: sign,
+    mode and argument.  The sign is \"+\" or \"-\".  The argument is
+    None if mode isn't one of \"b\", \"k\", \"l\", \"v\" or \"o\".
+
+    Example:
+
+    >>> irclib.parse_channel_modes(\"+ab-c foo\")
+    [['+', 'a', None], ['+', 'b', 'foo'], ['-', 'c', None]]
+    """
+
+    return _parse_modes(mode_string, "bklvo")
+
+def _parse_modes(mode_string, unary_modes=""):
+    """[Internal]"""
+    modes = []
+    arg_count = 0
+
+    # State variable.
+    sign = ""
+
+    a = mode_string.split()
+    if len(a) == 0:
+        return []
+    else:
+        mode_part, args = a[0], a[1:]
+
+    if mode_part[0] not in "+-":
+        return []
+    for ch in mode_part:
+        if ch in "+-":
+            sign = ch
+        elif ch == " ":
+            collecting_arguments = 1
+        elif ch in unary_modes:
+            if len(args) >= arg_count + 1:
+                modes.append([sign, ch, args[arg_count]])
+                arg_count = arg_count + 1
+            else:
+                modes.append([sign, ch, None])
+        else:
+            modes.append([sign, ch, None])
+    return modes
