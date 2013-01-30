4f505a6382c28a0c27932595119028ea3009ecb8
Vin <bevin@jabana.se>
2013-01-30 19:02:37 +0100

    New branch for queue upgrade

---
+++ hanyuu/queue/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+from .. import config
+import logging
+
+
+logger = logging.getLogger('hanyuu.queue')
+
+
+class Queue(object):
+    pass
