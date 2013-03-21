0072ece5859b0fbeb38b18c632fccde58d2548d4
Wessie <wessie@r-a-d.io>
2013-03-21 23:54:21 +0100

    Changed the configuration loading and API
    The new API relies on a global Settings object, which is located under
    `hanyuu.settings`.
    
    Settings can be accessed by using the dotted attribute way, so for
    example getting the database hostname you would do
    `hanyuu.settings.database.host` underlying is a simple dict subclass.
    
    Changed example configuration file to YAML format.

--- hanyuu/__init__.py
+++ hanyuu/__init__.py
@@ -1,14 +1,9 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
-from . import config
-import os
 import logging
+import hanyuu.config
 
-import sys
 
 logger = logging.getLogger('hanyuu')
 
-home = os.path.expanduser('~/.hanyuu')
-relative = os.path.abspath('./.hanyuu')
-config.load_configuration([home, relative])
